d1f1b16114a449f6fb2fd8965b486f720699047d
jrtx0 <1511198943@qq.com>
2023-10-12 08:52:51 +0000

    keep align with leetcode editor

--- leetcode/1.两数之和.rkt
+++ leetcode/1.两数之和.rkt
@@ -1,13 +1,15 @@
 #lang racket
 
-(define (two-sum nums target)
+(define/contract (two-sum nums target)
+  (-> (listof exact-integer?) exact-integer? (listof exact-integer?))
   (define table (make-hash))
   (for/last ([i (in-naturals)]
         [num (in-list nums)]
         #:final (hash-has-key? table (- target num)))
     (when (not (hash-has-key? table num))
         (hash-set! table num i))
-    (list (hash-ref table (- target num) '()) i)))
+    (list (hash-ref table (- target num) '()) i))
+  )
 
 
 (two-sum (list 2 7 11 15) 9)
