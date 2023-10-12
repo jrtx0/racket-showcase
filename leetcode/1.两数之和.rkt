2926c1f38ea14417f663ae2c64212583aee76b02
jrtx0 <jirentianxiang1024@gmail.com>
2023-10-12 16:43:54 +0800

    add leetcode two-sum solution

---
+++ leetcode/1.两数之和.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+(define (two-sum nums target)
+  (define table (make-hash))
+  (for/last ([i (in-naturals)]
+        [num (in-list nums)]
+        #:final (hash-has-key? table (- target num)))
+    (when (not (hash-has-key? table num))
+        (hash-set! table num i))
+    (list (hash-ref table (- target num) '()) i)))
+
+
+(two-sum (list 2 7 11 15) 9)
+(two-sum (list 3 2 4) 6)
+(two-sum (list 3 3) 6)
