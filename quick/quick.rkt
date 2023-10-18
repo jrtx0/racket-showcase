92d6840f9f7a8aa818bcdc372b65d6f286ceaad7
jrtx0 <jirentianxiang1024@gmail.com>
2023-10-18 23:02:15 +0800

    update quick.rkt

--- quick/quick.rkt
+++ quick/quick.rkt
@@ -63,3 +63,65 @@
 (filled-flash 40 30)
 
 (provide rainbow square)
+
+(require slideshow/code)
+
+(code (circle 10))
+
+(define-syntax pict+code
+  (syntax-rules ()
+    [(pict+code expr)
+     (hc-append 10
+                expr
+                (code expr))]))
+
+(pict+code (circle 10))
+
+(require racket/class
+         racket/gui/base)
+
+(define f (new frame% [label "My Art"]
+               [width 300]
+               [height 300]
+               [alignment '(center center)]))
+
+(send f show #t)
+
+(define (add-drawing p)
+  (let ([drawer (make-pict-drawer p)])
+    (new canvas% [parent f]
+         [style '(border)]
+         [paint-callback (lambda (self dc)
+                           (drawer dc 0 0))])))
+
+
+(add-drawing (pict+code (circle 10)))
+
+(add-drawing (colorize (filled-flash 50 30) "yellow"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
