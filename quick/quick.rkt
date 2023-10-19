9333cd6dc16812da800b61adb2a03aaae18a1a7a
jrtx0 <jirentianxiang1024@gmail.com>
2023-10-19 17:43:29 +0800

    update quick

--- quick/quick.rkt
+++ quick/quick.rkt
@@ -1,21 +1,43 @@
 #lang slideshow
 
+; 5
+; "art gallery"
+
+; (cirlce 10)
+
+; (rectangle 10 20)
+
+; (circle 10 20)
+
+; (hc-append (circle 10) (rectangle 10 20))
+
 (define c (circle 10))
 (define r (rectangle 10 20))
+
+; r
+; (hc-append c r)
+; (hc-append 20 c r c)
 
 (define (square n)
   ; A semi-colon starts a line comment.
   ; The expression below is the function body.
   (filled-rectangle n n))
 
+; (square 10)
+
 (define (four p)
   (define two-p (hc-append p p))
   (vc-append two-p two-p))
+
+; (four (cirlce 10))
 
 (define (checker p1 p2)
   (let ([p12 (hc-append p1 p2)]
         [p21 (hc-append p2 p1)])
     (vc-append p12 p21)))
+
+; (checker (colorize (square 10) "red")
+;          (colorize (square 10) "black"))
 
 (define (checkerboard p)
   (let* ([rp (colorize p "red")]
@@ -24,8 +46,15 @@
          [c4 (four c)])
     (four c4)))
 
+; (checkerboard (square 10))
+
+; circle
+
 (define (series mk)
   (hc-append 4 (mk 5) (mk 10) (mk 20)))
+
+; (series circle)
+; (series square)
 
 ; (series (lambda (size) (checkerboard (square size))))
 
@@ -41,28 +70,38 @@
    (series (lambda (sz) (colorize (mk sz) "green")))
    (series (lambda (sz) (colorize (mk sz) "blue")))))
 
+; (rgb-series circle)
+; (rgb-series square)
+
 (define (rgb-maker mk)
   (lambda (sz)
     (vc-append (colorize (mk sz) "red")
                (colorize (mk sz) "green")
                (colorize (mk sz) "blue"))))
 
-(list "red" "green" "blue")
+; (series (rgb-maker circle))
+; (series (rgb-maker square))
 
-(list (circle 10) (square 10))
+; (list "red" "green" "blue")
+
+; (list (circle 10) (square 10))
 
 (define (rainbow p)
   (map (lambda (color)
          (colorize p color))
        (list "red" "orange" "yellow" "green" "blue" "purple")))
 
-(apply vc-append (rainbow (square 5)))
+; (rainbow (square 5))
+
+; (apply vc-append (rainbow (square 5)))
 
 (require pict/flash)
 
-(filled-flash 40 30)
+; (filled-flash 40 30)
 
 (provide rainbow square)
+
+
 
 (require slideshow/code)
 
